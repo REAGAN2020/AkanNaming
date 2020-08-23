9926a4a2ec202683574705e2e51958103734d2e7
REAGAN2020 <reaganteflon@gmail.com>
2020-08-24 01:05:38 +0300

    deleted some items

--- bootstrap-4.5.2-dist/js/myscripts.js
+++ bootstrap-4.5.2-dist/js/myscripts.js
@@ -3,12 +3,16 @@ var akanFemaleNaming = ["Akosua", "Adwoa", "Abenaa",  "Akua", "Yaa","Afua","Ama"
 function akan() {
 	var date = document.getElementById('date').value;
     var gender = document.getElementById('gender').value;
-    alert(gender)
-    alert(date)
+    // alert(gender)
+    // alert(date)
     var CC = parseInt(date.substr(0, 2))
     var DD = parseInt(date.substr(8, 9))
     var MM = parseInt(date.substr(5, 7))
     var YY = parseInt(date.substr(2, 4))
     var dayOfTheWeek = parseInt((CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7);
-	alert(dayOfTheWeek);
+    // if (gender === "Male") {
+    //     alert("Ghanian name: " + akanMaleNaming[dayOfTheWeek])
+    // } else if (gender === "Female") {
+    //     alert("" + akanFemaleNaming[dayOfTheWeek])
+    // }
 }
