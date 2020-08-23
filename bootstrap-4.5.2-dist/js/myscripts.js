bc9ba492d90b1e9362c442a1179cac4923526f7f
REAGAN2020 <reaganteflon@gmail.com>
2020-08-23 18:07:59 +0300

    added var for naming male and female

---
+++ bootstrap-4.5.2-dist/js/myscripts.js
@@ -0,0 +1,16 @@
+var akanMaleNaming = ['Kwasi, Kwadwo, Kwabena,Kwaku, Yaw, Kofi,Kwame'];
+var akanFemaleNaming = ['Akosua, Adwoa, Adwoa, Adwoa, Yaa,Afua,Ama'];
+function akan() {
+	var date = document.getElementById('date').value;
+    var gender = document.getElementById('gender').value;
+    // alert(gender)
+    alert(date)
+    // var CC = parseInt(date.substr(0, 2))
+    // var DD = parseInt(date.substr(8, 9))
+    alert(DD)
+	// var mm;
+	// var yy;
+	// var dayOfTheWeek =
+	// 	(CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+	// alert(date);
+}
