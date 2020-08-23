167192367d3ea602a285b0fc10676a16602255f1
REAGAN2020 <reaganteflon@gmail.com>
2020-08-23 18:27:37 +0300

    added alert

--- bootstrap-4.5.2-dist/js/myscripts.js
+++ bootstrap-4.5.2-dist/js/myscripts.js
@@ -11,5 +11,5 @@ function akan() {
     var yy = parseInt(date.substr(2, 4))
 	var dayOfTheWeek =
 		(CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
-	// alert(date);
+	alert(date);
 }
