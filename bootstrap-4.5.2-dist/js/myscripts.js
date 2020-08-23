996e4dd77cfda9487b379680a3acafd5f51d64e6
REAGAN2020 <reaganteflon@gmail.com>
2020-08-24 00:31:35 +0300

    css stylings

--- bootstrap-4.5.2-dist/js/myscripts.js
+++ bootstrap-4.5.2-dist/js/myscripts.js
@@ -1,5 +1,5 @@
-var akanMaleNaming = ['Kwasi, Kwadwo, Kwabena,Kwaku, Yaw, Kofi,Kwame'];
-var akanFemaleNaming = ['Akosua, Adwoa, Adwoa, Adwoa, Yaa,Afua,Ama'];
+var akanMaleNaming = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"];
+var akanFemaleNaming = ["Akosua", "Adwoa", "Abenaa",  "Akua", "Yaa","Afua","Ama"];
 function akan() {
 	var date = document.getElementById('date').value;
     var gender = document.getElementById('gender').value;
@@ -7,9 +7,8 @@ function akan() {
     alert(date)
     var CC = parseInt(date.substr(0, 2))
     var DD = parseInt(date.substr(8, 9))
-    var mm = parseInt(date.substr(5, 7))
-    var yy = parseInt(date.substr(2, 4))
-	var dayOfTheWeek =
-		(CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
-	alert(date);
+    var MM = parseInt(date.substr(5, 7))
+    var YY = parseInt(date.substr(2, 4))
+    var dayOfTheWeek = parseInt((CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7);
+	alert(dayOfTheWeek);
 }
