ba0f14b7a4646e469626d6d180425a46c1429d50
REAGAN2020 <reaganteflon@gmail.com>
2020-08-23 18:24:07 +0300

    added data in var yy

--- bootstrap-4.5.2-dist/js/myscripts.js
+++ bootstrap-4.5.2-dist/js/myscripts.js
@@ -7,9 +7,9 @@ function akan() {
     alert(date)
     var CC = parseInt(date.substr(0, 2))
     var DD = parseInt(date.substr(8, 9))
-    alert(DD)
-	// var mm;
-	// var yy;
+    var mm = parseInt(date.substr(5, 7))
+    var yy = parseInt(date.substr(2, 4))
+    alert(yy)
 	// var dayOfTheWeek =
 	// 	(CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
 	// alert(date);
